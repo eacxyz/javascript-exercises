39e5c40461ebb7b577975a472fb8e7a1826cb0be
Noah <100672306+eacxyz@users.noreply.github.com>
2023-10-17 11:09:20 -0400

    Complete exercise 04
    My initial attempt, which is different from solution. It still works, but solution is shorter and much cleaner code.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+	let len = arr.length;
+	for (let i = 0; i < len; i++) {
+		for (let j = 0; j < args.length; j++) {
+			if (arr[i] === args[j]) {
+				arr.splice(i, 1);
+				i--;
+				len--;
+			}
+		}
+	}
+	return arr;
 };
 
 // Do not edit below this line
