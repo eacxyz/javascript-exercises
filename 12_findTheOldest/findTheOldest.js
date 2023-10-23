862dd86f63c1169aeb4fe23943592cc655bd26d2
Noah <100672306+eacxyz@users.noreply.github.com>
2023-10-23 18:41:34 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	let ages = people.sort((a, b) => {
+		const ageA = getAge(a);
+		const ageB = getAge(b);
+		return ageA > ageB ? -1 : 1;
+	});
+	return ages[0];
 };
+
+const getAge = function(person) {
+	let death;
+	let birth = person.yearOfBirth;
+	if ("yearOfDeath" in person == false) {
+		death = new Date().getFullYear();
+	} else {
+		death = person.yearOfDeath;
+	}
+	return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
