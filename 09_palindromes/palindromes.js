94a0efc4874a44c637ed7919b30cbbde7de3a7c8
Noah <100672306+eacxyz@users.noreply.github.com>
2023-10-23 12:38:14 -0400

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	let lower = str.toLowerCase();
+	let arr = lower.split("");
+	let pal = arr.filter(item => /[^.,!?\W]/.test(item));
+	let tmp = pal;
+	if (tmp.join("") === pal.reverse().join("")) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
