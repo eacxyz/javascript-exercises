55ec7d311707b73907b42b28dcd9ba84ad9f053b
Noah <100672306+eacxyz@users.noreply.github.com>
2023-10-23 18:04:56 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+	if (n == 1 || n == 2) return 1;
+	if (n <= 0) return "OOPS";
+	let arr = [1, 1];
+	for (let i = 2; i < n; i++) {
+		arr.push(arr[i-2]+arr[i-1]);
+	}
+	return arr[n-1];
 };
 
 // Do not edit below this line
